67d95df464e056108e7bf725fa3ebf0c72de0d44
Abhishek Choudhary <choudharyabhishek2199@gmail.com>
2022-06-19 12:08:56 +0530

    added track order page

---
+++ pages/components/track-order.js
@@ -0,0 +1,143 @@
+import React, { useEffect, useState } from "react";
+import {
+  ArrowBackIcon,
+  Box,
+  Center,
+  HStack,
+  Text,
+  VStack,
+  Image,
+  CheckCircleIcon,
+  Divider,
+  ShareIcon,
+  FavouriteIcon,
+  InfoIcon
+} from "native-base";
+import { setIconFunction } from "..";
+export default function TrackOrder() {
+    const iconList=[{
+        name:<ShareIcon size='sm' color='#9CA3AF'/>
+      },
+      {
+        name:<FavouriteIcon size='sm' color='#9CA3AF'/>
+      },
+      {
+        name:<InfoIcon size='sm' color='#9CA3AF'/>
+      }
+      ]
+    useEffect(() => {
+        setIconFunction(iconList)
+    },[]);
+  return (
+    <Box w="100vh">
+      <VStack>
+        <HStack alignItems="center" m="8" space={2}>
+          <ArrowBackIcon style={{ color: "black" }} />
+          <Text fontWeight={500}>Track Order</Text>
+        </HStack>
+        <Box
+          _light={{ bg: "white" }}
+          _dark={{ bg: "#1F2937" }}
+          h="80vh"
+          w="80vw"
+          mx="2.5vw"
+          alignItems="center"
+        >
+          <VStack space={8} w="60vw" mx='4vw' mt='4vh'>
+            <Box
+              borderRadius={8}
+              _light={{ bg: "#F3F4F6" }}
+              _dark={{ bg: "#374151" }}
+            //   mt="4vh"
+            //   mx="4vw"
+            //   w="60vw"
+            >
+              <HStack space={4}>
+                <Image
+                  margin={4}
+                  h="90"
+                  w="74"
+                  source={{
+                    uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Woman_with_a_blue_scarf_%28Unsplash%29.jpg/320px-Woman_with_a_blue_scarf_%28Unsplash%29.jpg",
+                  }}
+                  alt="order image"
+                />
+                <VStack justifyContent="space-evenly">
+                  <Box>
+                    <Text fontWeight={500}>Sweater Dress</Text>
+                    <Text>Girl Self Design</Text>
+                  </Box>
+                  <Box>
+                    <Text fontWeight={500}>Rs 1999</Text>
+                  </Box>
+                </VStack>
+              </HStack>
+            </Box>
+            <VStack>
+              <VStack space={2}>
+                <HStack space={4}>
+                    <Box zIndex='-1'>
+                        <CheckCircleIcon m="1" _light={{color:"#4C1D95"}} _dark={{color:'#8B5CF6'}} size="sm" z-index='-1' />
+                    </Box>
+                    <Divider zIndex='-2' ml='-7' mt='4' _light={{bg:"#4C1D95"}} _dark={{bg:'#8B5CF6'}} orientation="vertical" />
+                  <Box>
+                    <Text fontWeight={600}>Order Placed</Text>
+                    <Text fontWeight={500}>
+                      We have received your order on 28-May-2021.
+                    </Text>
+                    <Text fontWeight={400}>5:38PM</Text>
+                  </Box>
+                </HStack>
+                <HStack space={4}>
+                <Box zIndex='-1'>
+                        <CheckCircleIcon m="1" _light={{color:"#4C1D95"}} _dark={{color:'#8B5CF6'}} size="sm" z-index='-1' />
+                    </Box>
+                    <Divider zIndex='-2' ml='-7' mt='4' _light={{bg:"#4C1D95"}} _dark={{bg:'#8B5CF6'}} orientation="vertical" />
+                  <Box>
+                    <Text fontWeight={600}>Order Packed</Text>
+                    <Text fontWeight={500}>
+                      Seller has processed your order on 29th. Your item has
+                      been picked up by courier partner on 30 May-2021.
+                    </Text>
+                    <Text fontWeight={400}>5:38PM</Text>
+                  </Box>
+                </HStack>
+                <HStack space={4}>
+                <Box zIndex='-1'>
+                        <CheckCircleIcon m="1" _light={{color:"#C4B5FD"}} _dark={{color:'#9CA3AF'}} size="sm" z-index='-1' />
+                    </Box>
+                    <Divider borderStyle='dashed' borderColor="#8B5CF6" _light={{borderColor:"#C4B5FD", bg:"white"}} _dark={{borderColor:'#D1D5DB', bg:"#1F2937"}} borderWidth='1' borderRadius='1' zIndex='-2' ml='-7' mt='4' orientation="vertical" />
+                  <Box>
+                    <Text color="#6B7280" fontWeight={600}>
+                      Shipped
+                    </Text>
+                    <Text color="#6B7280" fontWeight={500}>
+                      Your item has been picked up by courier partner on 30
+                      May-2021.
+                    </Text>
+                    <Text color="#6B7280" fontWeight={400}>
+                      5:38PM
+                    </Text>
+                  </Box>
+                </HStack>
+                <HStack space={4}>
+                <Box zIndex='-1'>
+                        <CheckCircleIcon m="1" _light={{color:"#C4B5FD"}} _dark={{color:'#9CA3AF'}} size="sm" z-index='-1' />
+                    </Box>
+                  <Box>
+                    <Text color="#6B7280" fontWeight={600}>
+                      Out For Delivery
+                    </Text>
+                    <Text color="#6B7280" fontWeight={500}>
+                      Your order is out for delivery.
+                    </Text>
+                  </Box>
+                </HStack>
+              </VStack>
+            </VStack>
+          </VStack>
+        </Box>
+      </VStack>
+    </Box>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -10,34 +10,61 @@ import {
   FlatList,
   ShareIcon,
   Divider,
+  FavouriteIcon,
+  InfoIcon,
+  Icon
 } from "native-base";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import TrackOrder from "./components/track-order";
+import {PersonIcon} from '@mui/icons-material/Person';
+
+export let setIconFunction = (iconList) => {};
+export let setScreenName = (screenName) => {};
+
 export default function App() {
   const { toggleColorMode } = useColorMode();
+  const [icons,setIcons] = useState([])
+  const [ screen, setScreen] = useState(<TrackOrder/>);
+
+  setIconFunction = (iconList)=>{
+    setIcons(iconList)
+  }
+
+  setScreenName = (screenName)=>{
+    setScreen(screenName)
+  }
   const data = [
     {
       name: "Contact",
+      icon: "PersonIcon",
     },
     {
       name: "Group",
+      icon: "GroupIcon",
     },
     {
       name: "Notifications",
+      icon: "NotificationsIcon",
     },
     {
       name: "Order",
+      icon: "ShoppingBagIcon",
     },
     {
       name: "Settings",
+      icon: "SettingsIcon",
     },
     {
       name: "Privacy Policies",
+      icon: "Policy",
     },
     {
       name: "Help and Support",
+      icon: "SupportAgentIcon",
     },
     {
       name: "Refer and Earn",
+      icon: "ShareIcon",
     },
   ];
   return (
@@ -60,11 +87,16 @@ export default function App() {
               px={4}
               onPress={toggleColorMode}
             >
-              <span style={{ color: "#8B5CF6" }}>HackWith</span>NativeBase
+              <Text color="#8B5CF6">HackWith</Text>NativeBase
             </Text>
           </HStack>
           <HStack>
+            {icons.map((icon,index)=>{
+              return <Box alignContent='center' justifyContent='center' m='2'>{icon.name}</Box>
+            })
+            }
             <Avatar
+              ml='2'
               size="sm"
               source={{
                 uri: "https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
@@ -83,8 +115,9 @@ export default function App() {
           <Box
             shadow={2}
             h="25vh"
+            alignItems='center'
           >
-            <Center flex={1}>
+            {/* <Center flex={1}> */}
               <Avatar
                 size="xl"
                 m={1}
@@ -100,7 +133,7 @@ export default function App() {
               <Text fontFamily="body" color="gray.500" fontSize="xs">
                 janedoe2@mydomain.com
               </Text>
-            </Center>
+            {/* </Center> */}
           </Box>
           <Divider/>
           <Box
@@ -114,7 +147,6 @@ export default function App() {
               renderItem={({ item }) => (
                 <Box borderRadius={4} pl="4" pr="5" py="3">
                   <HStack space={4} ml={2} alignItems="center" flex={1}>
-                    <ShareIcon />
                     <Text
                       _dark={{
                         color: "warmGray.50",
@@ -143,7 +175,8 @@ export default function App() {
         </Box>
         {/*Content Box*/}
         <Box> 
-          Hello
+          {screen}
+          {/* Hello */}
         </Box>
       </Box>
     </Box>
