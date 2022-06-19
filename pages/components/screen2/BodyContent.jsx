b898e004f19d43fa805de16a0f40f4141ef836de
Anshuman Narayan <anshumanjf@gmail.com>
2022-06-19 13:12:17 +0530

    screen 2 basic

--- pages/components/screen2/BodyContent.jsx
+++ pages/components/screen2/BodyContent.jsx
@@ -10,16 +10,73 @@ import {
     Heading,
     Divider,
     Button,
+    FavouriteIcon,
 } from "native-base";
 import Image from 'next/image'
 
 import img from '../../../public/images/screen2.jpg';
 
 const BodyContent = () => {
+    let buttonTextAttr = {
+        color: "#1F2937",
+        fontSize: "md",
+        fontWeight: "420",
+        fontSize: "20"
+    }
     return (
-        <Box flex="1" safeAreaTop>
+
+        <Box bgColor="#FFFFFF" ml="5">
+
+            <HStack>
+                <Box>
+                    <Image src={img} alt="screen2.jpg" />
+                </Box>
+                <Box>
+                    <Text fontWeight="510" fontSize="18px">Body Suit</Text>
+                    <Text fontWeight="400" fontSize="16px" color="#6B7280">Mother care</Text>
+                    <Text fontWeight="510" fontSize="20px" color="#1F2937">₹500</Text>
+                    <HStack space="12vw" mt="10px">
+                        <Text fontWeight="510" fontSize="18px" color="#1F2937" height="21px">Select Size <Text color='#6B7280'>(Age Group)</Text></Text>
+                        <Text fontWeight="510" fontSize="18px" color="#4C1D95" height="21px">Size Chart</Text>
+                    </HStack>
+                    <HStack space={5} mt="5">
+                        <Button height="4vw" bgColor="#F5F3FF" alignItems="center" _text={buttonTextAttr}>new born</Button>
+                        <Button bgColor="#F5F3FF" alignItems="center" _text={buttonTextAttr}>Tiny Baby</Button>
+                        <Button bgColor="#F5F3FF" alignItems="center" _text={buttonTextAttr}>0-3 M</Button>
+                    </HStack>
+                    <HStack space={10} mt="4">
+                        <Box bg="#F5F3FF" h="4vw" w="4vw" alignItems="center" justifyContent="center"><FavouriteIcon size="9" /></Box>
+                        <Button width="25vw" bgColor="#4C1D95" size="lg" _text={{
+                            color: "#F9FAFB",
+                            fontWeight: "520",
+                            fontSize: "22"
+                        }}>CONTINUE</Button>
+                    </HStack>
+                    <HStack space={10} mt="6">
+                        <Box justifyContent="center" alignItems="center" h="6vh" w="10vw" borderBottomWidth="5px" borderBottomColor="#4C1D95">
+                            <Text fontWeight="510" fontSize="18px" color="#4C1D95" height="21px">Description</Text>
+                        </Box>
+                        <Box justifyContent="center" alignItems="center" h="6vh" w="10vw" >
+                            <Text fontWeight="510" fontSize="18px" color="#6B7280" height="21px">Reviews</Text>
+                        </Box>
+                    </HStack>
+                    <Box maxWidth="33vw" mt="4">Yellow bodysuit, has a round neck with envelope detail along the shoulder, short sleeves and snap button closures along the crotch. Yellow bodysuit, has a round neck with envelope detail along the shoulder, short sleeves and snap button closures along the crotch.Yellow bodysuit, has a round neck with envelo</Box>
+
+                </Box>
+            </HStack>
+
+        </Box>
+
+    )
+}
+
+export default BodyContent
+
+/*
+
+<Box flex="1" safeAreaTop>
             <ScrollView>
-                <VStack space={2.5} w="100%" px="3" mt="5">
+                <VStack space={2.5} w="100%" px="3" mt="2">
                     <Flex direction="row" mb="2.5" mt="1.5" justifyContent="left">
                         <Box>
                             <Image src={img} alt="screen2.jpg" />
@@ -28,19 +85,37 @@ const BodyContent = () => {
                             <Text fontWeight="510" fontSize="18px">Body Suit</Text>
                             <Text fontWeight="400" fontSize="16px" color="#6B7280">Mother care</Text>
                             <Text fontWeight="510" fontSize="20px" color="#1F2937">₹500</Text>
-                            <Text mt="10px" fontWeight="510" fontSize="16px" color="#1F2937" height="21px">Select Size <span color='#6B7280'>(Age Group)</span></Text>
-                            <Flex direction='row' mt="10" justifyContent="space-between">
-                                <Button alignItems="center">new born</Button>
-                                <Button alignItems="center">Tiny Baby</Button>
-                                <Button alignItems="center">0-3 M</Button>
-                            </Flex>
+                            <HStack space="12vw" mt="10px">
+                                <Text fontWeight="510" fontSize="18px" color="#1F2937" height="21px">Select Size <Text color='#6B7280'>(Age Group)</Text></Text>
+                                <Text fontWeight="510" fontSize="18px" color="#4C1D95" height="21px">Size Chart</Text>
+                            </HStack>
+                            <HStack space={5} mt="5">
+                                <Button height="4vw" bgColor="#F5F3FF" alignItems="center" _text={buttonTextAttr}>new born</Button>
+                                <Button bgColor="#F5F3FF" alignItems="center" _text={buttonTextAttr}>Tiny Baby</Button>
+                                <Button bgColor="#F5F3FF" alignItems="center" _text={buttonTextAttr}>0-3 M</Button>
+                            </HStack>
+                            <HStack space={10} mt="4">
+                                <Box bg="#F5F3FF" h="4vw" w="4vw" alignItems="center" justifyContent="center"><FavouriteIcon size="9" /></Box>
+                                <Button width="25vw" bgColor="#4C1D95" size="lg" _text={{
+                                    color: "#F9FAFB",
+                                    fontWeight: "520",
+                                    fontSize: "22"
+                                }}>CONTINUE</Button>
+                            </HStack>
+                            <HStack space={10} mt="6">
+                                <Box justifyContent="center" alignItems="center" h="6vh" w="10vw" borderBottomWidth="5px" borderBottomColor="#4C1D95">
+                                    <Text fontWeight="510" fontSize="18px" color="#4C1D95" height="21px">Description</Text>
+                                </Box>
+                                <Box justifyContent="center" alignItems="center" h="6vh" w="10vw" >
+                                    <Text fontWeight="510" fontSize="18px" color="#6B7280" height="21px">Reviews</Text>
+                                </Box>
+                            </HStack>
+                            <Box maxWidth="33vw" mt="4">Yellow bodysuit, has a round neck with envelope detail along the shoulder, short sleeves and snap button closures along the crotch. Yellow bodysuit, has a round neck with envelope detail along the shoulder, short sleeves and snap button closures along the crotch.Yellow bodysuit, has a round neck with envelo</Box>
+                            <p></p>
                         </Box>
                     </Flex>
                 </VStack>
             </ScrollView>
-        </Box>
-    )
-}
+        </Box >
 
-export default BodyContent
-
+*/
--- pages/components/screen2/MainBox.jsx
+++ pages/components/screen2/MainBox.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import { Box, Text, Center, Container, Heading } from "native-base";
+import { Box, Text, Center, Container, Heading, VStack } from "native-base";
 import BodyContent from './BodyContent';
 import ArrowBackIcon from "react-native-svg";
 import { AiOutlineArrowLeft } from "react-icons/ai";
 
 const MainBox = () => {
     return (
-        <div style={{ height: "100vh", alignSelf: "center", width: "80%", backgroundColor: "red", marginTop: "10vh" }}>
-            <Text fontFamily='SF pro' fontSize="20" mt="50px" ml="30"><AiOutlineArrowLeft />  Body Suit</Text>
+        <VStack>
+            <Text fontFamily='SF pro' fontSize="20" mt="6vh" ml="30"><AiOutlineArrowLeft />  Body Suit</Text>
             <BodyContent />
-        </div>
+        </VStack>
     )
 }
 
